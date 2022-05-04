14dd26bd5f1e038ef36781eaf55b0bdfe72ee72d
lif314 <2216155077@qq.com>
2022-05-04 18:42:01 +0800

    vuex模块开发

---
+++ src/store/search/index.js
@@ -0,0 +1,28 @@
+// search模块的仓库
+// state: 仓库存储数据
+const state = {
+
+}
+
+// mutations: 修改state的唯一手段
+const mutations = {
+
+}
+
+// action: 处理action，可以书写业务逻辑，处理异步
+const actions = {
+
+}
+
+// getters: 类似计算属性，用于简化仓库数据，让组件获取仓库的数据更加方便
+const getters = {
+
+}
+
+// search模块的仓库信息
+export default{
+    state,
+    mutations,
+    actions,
+    getters
+}
