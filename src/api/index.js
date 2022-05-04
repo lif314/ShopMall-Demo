15f1b8ffbf5709e8e305222870accfa711e799d7
lif314 <2216155077@qq.com>
2022-05-04 23:57:10 +0800

    mock模拟获取数据

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 // API接口统一管理
 import requests from "./request";
+import mockRequests from './mockAjax'
 
 // 三级分类 /api/product/getBaseCategoryList GET
 export const getBaseCategoryList = ()=>{
@@ -9,3 +10,10 @@ export const getBaseCategoryList = ()=>{
         method: 'get'
     })
 }
+
+
+// mock获取模拟数据 拦截ajax请求
+export const reqGetBannerList = ()=>mockRequests.get('/banner')
+
+// mock获取模拟数据 拦截ajax请求
+export const reqGetFloorrList = ()=>mockRequests.get('/floor')
---
+++ src/api/mockAjax.js
@@ -0,0 +1,34 @@
+// mock请求
+import axios from "axios";
+// 引入进度条
+import nprogress from "nprogress";
+// 引入样式 可以自己更改样式
+import "nprogress/nprogress.css";
+
+// 1、利用axios创建一个axios实例
+const requests = axios.create({
+    baseURL:"/mock",
+    timeout: 5000, // 请求超时时间5s
+})
+
+
+// 2、请求拦截器: 检测到需要发出去的请求
+requests.interceptors.request.use((config)=>{
+    // config配置对象，headers请求头比较重要
+    // 启动nprocess进度条
+    nprogress.start()
+    return config
+})
+
+// 3、响应拦截器
+requests.interceptors.response.use((res)=>{
+    // 成功的回调函数
+    // nprocess进度条结束
+    nprogress.done()
+    return res.data; // 直接返回数据
+},(error)=>{
+    // 失败的回调函数
+    return Promise.reject(new Error('faile')); // 终止Promise链
+})
+
+export default requests
